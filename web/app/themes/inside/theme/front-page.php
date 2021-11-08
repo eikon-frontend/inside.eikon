ee3915247f07c83d9680bc2cdd4388df29bfb41a
Julien Minguely <jminguely@gmail.com>
2021-11-08 11:46:15 +0100

    Adds frontpage

---
+++ web/app/themes/inside/theme/front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for the front page
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'front-page.twig', 'page.twig' ), $context );
