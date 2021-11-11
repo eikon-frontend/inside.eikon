43d1d0e2332d431c59150b4d6c870a7f6c06d9d8
Julien Minguely <jminguely@gmail.com>
2021-11-11 17:43:44 +0100

    Quick correction pass

--- web/app/themes/inside/assets/js/components/masonry.js
+++ web/app/themes/inside/assets/js/components/masonry.js
@@ -1,16 +1,21 @@
 import Masonry from 'masonry-layout';
 
 const masonry = () => {
-  var msnry = new Masonry('.grid', {
-    columnWidth: '.grid-sizer',
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    gutter: 20
-  });
+  [...document.querySelectorAll('.grid')].forEach(
+    (gridElement) => {
+      var msnry = new Masonry(gridElement, {
+        columnWidth: gridElement.querySelector('.grid-sizer')[0],
+        itemSelector: gridElement.querySelector('.grid-item')[0],
+        percentPosition: true,
+        gutter: 20
+      });
 
-  window.onload = (event) => {
-    msnry.layout();
-  };
+      window.onload = (event) => {
+        msnry.layout();
+      };
+    }
+  );
+
 };
 
 export default masonry;
