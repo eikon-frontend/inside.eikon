14b31665b31e80ad37afd9d36d95e93a2614a4a5
Julien Minguely <jminguely@gmail.com>
2021-11-17 12:23:30 +0100

    photoswipe, first pass

---
+++ web/app/themes/inside/assets/js/components/photoswipe.js
@@ -0,0 +1,45 @@
+import * as PhotoSwipe from 'photoswipe';
+import * as PhotoSwipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
+
+const gallery = () => {
+  const pswpElement = document.querySelectorAll('.pswp')[0];
+  const galleries = document.querySelectorAll('.swiper');
+  const options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0, // start at first slide
+    history: false,
+  };
+
+  const openPhotoSwipe = (psGallery) => {
+    const images = psGallery.querySelector('.swiper-slide').children;
+
+    console.log(images);
+
+    const galleryItems = [];
+    for (let i = 0; i < images.length; i += 1) {
+      galleryItems.push({
+        src: images[i].querySelector('img').src,
+        w: images[i].querySelector('img').dataset.w,
+        h: images[i].querySelector('img').dataset.h,
+      });
+    }
+
+    // Initializes and opens PhotoSwipe
+    const photoSwipeGallery = new PhotoSwipe(
+      pswpElement,
+      PhotoSwipeUIDefault,
+      galleryItems,
+      options,
+    );
+    photoSwipeGallery.init();
+  };
+
+  for (let i = 0; i < galleries.length; i += 1) {
+    console.log(galleries[i].querySelector('.open-gallery'));
+    galleries[i].querySelector('.open-gallery').onclick = () =>
+      openPhotoSwipe(galleries[i]);
+  }
+};
+
+export default gallery;
--- web/app/themes/inside/assets/js/index.js
+++ web/app/themes/inside/assets/js/index.js
@@ -1,11 +1,13 @@
 import 'bootstrap';
 
 import masonry from './components/masonry';
+import photoswipe from './components/photoswipe';
 import slider from './components/slider';
 import scrolltrigger from './components/scrolltrigger';
 
 const init = () => {
   masonry();
+  photoswipe();
   slider();
   scrolltrigger();
 };
