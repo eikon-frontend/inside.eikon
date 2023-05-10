3570ebba14ba83be0d71dc6f02b89a038af8fb6a
Julien Minguely <jminguely@gmail.com>
2023-05-10 13:39:00 +0200

    Adds roles management to the CMS

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,8 +1,7 @@
 <?php
+
 /**
  * Configuration overrides for WP_ENV === 'production'
  */
 
 use Roots\WPConfig\Config;
-
-Config::define('MATOMO_SUPPORT_ASYNC_ARCHIVING', false);
---
+++ web/app/themes/inside/theme/inc/roles.php
@@ -0,0 +1,12 @@
+<?php
+
+$wp_roles = new WP_Roles();
+$wp_roles->remove_role("matomo_write_role");
+$wp_roles->remove_role("matomo_view_role");
+$wp_roles->remove_role("matomo_superuser_role");
+$wp_roles->remove_role("matomo_admin_role");
+$wp_roles->remove_role("contributor");
+$wp_roles->remove_role("author");
+
+$wp_roles->add_role("supervisor", "Enseignant / enseignante");
+$wp_roles->add_role("responsable", "Responsable de branche");
