bda21ff89aceed268cf39d375bfa6160ba50a77f
Julien Minguely <jminguely@gmail.com>
2023-11-01 01:19:43 +0100

    Removes the footer menu

--- web/app/themes/inside/inc/menu.php
+++ web/app/themes/inside/inc/menu.php
@@ -4,6 +4,5 @@ function custom_nav()
 {
   register_nav_menu('MainNav', __('Menu principal'));
   register_nav_menu('SecondaryNav', __('Menu secondaire'));
-  register_nav_menu('FooterNav', __('Menu footer'));
 }
 add_action('init', 'custom_nav');
