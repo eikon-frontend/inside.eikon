2f34a2967b9945afad1b64b1ae26b635789bcec6
Julien Minguely <jminguely@gmail.com>
2023-11-17 16:33:47 +0100

    Adds a footer nav position

--- web/app/themes/inside/inc/menu.php
+++ web/app/themes/inside/inc/menu.php
@@ -4,5 +4,6 @@ function custom_nav()
 {
   register_nav_menu('MainNav', __('Menu principal'));
   register_nav_menu('SecondaryNav', __('Menu secondaire'));
+  register_nav_menu('FooterNav', __('Footer menu'));
 }
 add_action('init', 'custom_nav');
