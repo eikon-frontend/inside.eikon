8f57eace6dc83c4614b4c785c8d2f5059bca6cd4
Julien Minguely <jminguely@gmail.com>
2023-11-24 01:58:36 +0100

    Adds featured image in project admin

--- web/app/themes/inside/inc/admin.php
+++ web/app/themes/inside/inc/admin.php
@@ -7,3 +7,20 @@ function my_remove_admin_menus()
 {
   remove_menu_page('edit-comments.php');
 }
+
+
+add_filter('manage_project_posts_columns', 'add_image_column');
+function add_image_column($columns)
+{
+  $columns['image'] = __('Image');
+  return $columns;
+}
+
+add_action('manage_project_posts_custom_column', 'add_image_column_content', 10, 2);
+function add_image_column_content($column, $post_id)
+{
+  // Image column
+  if ('image' === $column) {
+    echo get_the_post_thumbnail($post_id, array(80, 80));
+  }
+}
