1a6bfcd8f58f060e799f9ff1aebc28c2cf3d92dc
Julien Minguely <jminguely@gmail.com>
2024-03-14 19:19:10 +0100

    First pass on themification of the gutenberg editor

--- web/app/themes/inside/inc/guntenberg.php
+++ web/app/themes/inside/inc/guntenberg.php
@@ -1,16 +1,22 @@
 <?php
-
-add_filter('allowed_block_types_all', 'eikon_allowed_block_types', 25, 2);
-
-function eikon_allowed_block_types($allowed_blocks, $editor_context)
+function enqueue_custom_fonts()
 {
-  return array(
-    'core/image',
-    'core/paragraph',
-    'core/heading',
-    'core/list',
-    'core/list-item',
-    'eikonblocks/test',
-    'eikonblocks/projects'
-  );
+  $theme_directory = get_template_directory_uri();
+  $css = "@font-face {
+            font-family: 'NoiGrotesk';
+            src: url('{$theme_directory}/assets/fonts/NoiGrotesk-Medium.woff2') format('woff2');
+            font-weight: 500;
+            font-style: normal;
+            font-display: swap;
+          }
+          @font-face {
+            font-family: 'NoiGrotesk';
+            src: url('{$theme_directory}/assets/fonts/NoiGrotesk-MediumItalic.woff2') format('woff2');
+            font-weight: 500;
+            font-style: italic;
+            font-display: swap;
+          }
+        ";
+  wp_add_inline_style('wp-editor', $css);
 }
+add_action('enqueue_block_editor_assets', 'enqueue_custom_fonts');
