a7c8f040665d11943a9e2e2720a6de6ff18ffd7c
Julien Minguely <jminguely@gmail.com>
2024-09-28 03:43:51 +0200

    Scaffold a department teaer block

--- web/app/mu-plugins/eikonblocks-departments-teaser/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-departments-teaser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'efadf194d514fe7164ae');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'c650771a7bc14d989c63');
