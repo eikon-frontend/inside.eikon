4546e76ccb895694a8d77ac16e14ef79b6ca7335
Julien Minguely <jminguely@gmail.com>
2024-09-29 11:04:09 +0200

    Finished the number block

--- web/app/mu-plugins/eikonblocks-departments-teaser/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-departments-teaser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'c650771a7bc14d989c63');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '50d1ba4af82006a256ec');
--- web/app/mu-plugins/eikonblocks-numbers/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-numbers/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '86db5111437fc4570f49');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c607a7e43c51f7601241');
