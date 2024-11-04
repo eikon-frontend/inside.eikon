ce5ab1a4bf2e0f91379bdb988a650831aa476343
Julien Minguely <jminguely@gmail.com>
2024-11-04 10:33:56 +0100

    no icon when undified

--- web/app/mu-plugins/eikonblocks-buttons/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-buttons/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '03d681fe566d45c162d1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '60b33261dd6dbd715291');
