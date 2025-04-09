06e975505ba7a7072519fbf198d75668a64b0be5
Julien Minguely <jminguely@gmail.com>
2025-04-09 14:52:32 +0200

    core and plugin update

--- web/app/mu-plugins/eikonblocks-buttons/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-buttons/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4d29c3f79e10aa5f846f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e6ebc677bbf6c47194a0');
