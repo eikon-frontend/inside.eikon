2d3fcf40414e70385a1426b271636ff5afe7f5dd
Julien Minguely <jminguely@gmail.com>
2025-05-23 10:19:27 +0200

    Adds a dismissible notice to informof the documentation

--- web/app/themes/inside/inc/headless.php
+++ web/app/themes/inside/inc/headless.php
@@ -35,3 +35,42 @@ function custom_rest_url($url)
 }
 
 add_filter('rest_url', 'custom_rest_url');
+
+function display_documentation_notice()
+{
+  // Check if the notice has been dismissed
+  if (get_user_meta(get_current_user_id(), 'inside_documentation_notice_dismissed', true)) {
+    return;
+  }
+
+?>
+  <div class="notice notice-info is-dismissible">
+    <p><strong>Documentation Inside.eikon.ch</strong></p>
+    <p>Pour savoir comment éditer le site et ajouter des projets au portfolio, consultez la documentation sur le
+      <a href="https://www.notion.so/eikon-imd/e071042b616246b68f3e811758305160?v=a8ccb994f5454e968d8b8d0e060978a6&pvs=4#1fc709599a81802fb975e14ace44b1c3"
+        target="_blank">notion de l'école
+      </a>
+    </p>
+  </div>
+  <script>
+    jQuery(document).ready(function($) {
+      $(document).on('click', '.notice-info .notice-dismiss', function() {
+        $.ajax({
+          url: ajaxurl,
+          data: {
+            action: 'dismiss_documentation_notice'
+          }
+        });
+      });
+    });
+  </script>
+<?php
+}
+
+function handle_documentation_notice_dismissal()
+{
+  update_user_meta(get_current_user_id(), 'inside_documentation_notice_dismissed', true);
+}
+
+add_action('admin_notices', 'display_documentation_notice');
+add_action('wp_ajax_dismiss_documentation_notice', 'handle_documentation_notice_dismissal');
