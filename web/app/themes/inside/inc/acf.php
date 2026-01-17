0e5b70be4e57e37ac5dd46e2fc5cdc7915a3b9d9
Julien Minguely <jminguely@gmail.com>
2026-01-17 14:50:39 +0100

    better portfolio system, yeah

--- web/app/themes/inside/inc/acf.php
+++ web/app/themes/inside/inc/acf.php
@@ -11,3 +11,20 @@ add_filter('acf/load_field/name=galerie', function ($field) {
   }
   return $field;
 });
+
+// Custom WYSIWYG toolbar for portfolio text layout
+add_filter('acf/fields/wysiwyg/toolbars', function ($toolbars) {
+  $toolbars['Portfolio Layout'] = [];
+  $toolbars['Portfolio Layout'][1] = ['formatselect', 'italic', 'link', 'bullist', 'numlist', 'undo', 'redo', 'fullscreen'];
+  return $toolbars;
+});
+
+// Restrict format dropdown to only paragraph and h3
+add_filter('acf/fields/wysiwyg/toolbars', function ($toolbars) {
+  return $toolbars;
+}, 20);
+
+add_filter('tiny_mce_before_init', function ($settings) {
+  $settings['block_formats'] = 'Paragraph=p;Heading 3=h3';
+  return $settings;
+});
