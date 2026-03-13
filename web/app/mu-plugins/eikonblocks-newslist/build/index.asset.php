94ee365d1bc2c55a6c3c18b16f6d968f3aa9a362
Julien Minguely <jminguely@gmail.com>
2026-03-13 19:34:34 +0100

    fixes the newslist plugin

---
+++ web/app/mu-plugins/eikonblocks-newslist/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '2f11f990aa63df348e1b');
--- web/app/themes/inside/functions.php
+++ web/app/themes/inside/functions.php
@@ -1,16 +1,4 @@
 <?php
-
-// Temporary debug: log which block triggers the namespace error
-add_action('init', function () {
-  add_filter('doing_it_wrong_trigger_error', function ($trigger, $function_name, $message) {
-    if ($function_name === 'WP_Block_Type_Registry::register') {
-      error_log('BLOCK NAMESPACE DEBUG: ' . $message);
-      error_log('BLOCK NAMESPACE TRACE: ' . (new \Exception())->getTraceAsString());
-    }
-    return $trigger;
-  }, 10, 3);
-}, 0);
-
 foreach (glob(get_template_directory() . "/inc/*.php") as $file) {
   require $file;
 }
