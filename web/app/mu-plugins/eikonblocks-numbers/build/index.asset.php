0852bb596ac406dfab0eda16524e4bb87e1c5c3d
Julien Minguely <jminguely@gmail.com>
2026-08-29 15:59:01 +0200

    fixes blocks layout

--- web/app/mu-plugins/eikonblocks-numbers/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-numbers/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '30225b7502caf9499efc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2b2d419492a2f7bc67aa');
--- web/app/mu-plugins/eikonblocks-projects/build/index.asset.php
+++ web/app/mu-plugins/eikonblocks-projects/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '1740fa94e8f97d1abb20');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'df053aca5caae2e4bc87');
